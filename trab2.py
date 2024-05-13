d9aaa94e0c7e25072e6fb0cbba039ced1eab20e0
znx999 <weslley.alves3310@gmail.com>
2024-05-13 11:10:31 -0300

    Loja de gelados

--- trab2.py
+++ trab2.py
@@ -1,6 +1,6 @@
 #Trabalho com estrutura de repetição usando uma loja de gelados
 
-#Mensagem de boas-vindas
+#Mensagem de boas-vindas | Menu
 print('-Bem-vindo a Loja de Gelados do Weslley Alves-')
 print('-' * 18, 'Cardápio', '-' * 18)
 print('-' * 46)
@@ -21,7 +21,7 @@ while True:
     #Recebe o sabor desejado do cliente
     sabor = input('Entre com o sabor desejado (CP/AC): ').upper()
         
-    #Verifica se o sabor esta incluso, caso não estiver executa o print e retorna ao inicio
+    #Verifica se o sabor esta incluso na lista, caso não estiver executa o print e retorna 
     if sabor not in ['CP', 'AC']:
         print('Sabor inválido. Tente novamente\n')
         continue
@@ -29,21 +29,21 @@ while True:
     #Recebe o tamanho desejado do cliente
     tamanho = input('Entre com o tamanho desejado (P/M/G): ').upper()
     
-    #Verifica se o tamanho  esta incluso, caso não estiver executa o print e retorna ao inicio
+    #Verifica se o tamanho  esta incluso na lista, caso não estiver executa o print e retorna 
     if tamanho not in ['P', 'M', 'G']:
         print('Tamanho inválido. Tente novamente\n')
         continue
     
     #Verifica qual sabor foi escolhido
     if sabor == 'CP':
-        #Verifica o tamanho solicitado e o total recebe a soma
+        #Verifica o tamanho solicitado e o valor recebe o valor do produto
         if tamanho == 'P':
             valor = 9
         elif tamanho == 'M':
             valor = 14
         elif tamanho == 'G':
             valor = 18
-        print(f'Você pediu um Cupuaçu no tamanho {tamanho}: R${valor}\n')
+        print(f'Você pediu um Cupuaçu no tamanho {tamanho}: R${valor:.2f}\n')
     
     if sabor == 'AC':
         if tamanho == 'P':
@@ -52,9 +52,9 @@ while True:
             valor = 16
         elif tamanho == 'G':
             valor = 20
-        print(f'Você pediu um Açaí no tamanho {tamanho}: R${valor}\n')
+        print(f'Você pediu um Açaí no tamanho {tamanho}: R${valor:.2f}\n')
     
-    #Conta para no fim mostrar o total a ser pago    
+    #Total recebe o valor do produto, caso for escolhido mais de uma quantidade o total soma ele mesmo mais o valor
     total += valor
      
     #Print perguntando se o usuario deseja algo a mais
@@ -67,7 +67,7 @@ while True:
         break
     
 #Ao escolher 'N' e mostrado o valor total a ser pago
-print(f'\nO valor total a ser pago: R${total}')
+print(f'\nO valor total a ser pago: R${total:.2f}')
                 
 
                 
